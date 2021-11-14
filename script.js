dd77d4caab5c158fd0c301467ab9e531b52788ec
isabellagh <isabellagh88@gmail.com>
2021-11-13 21:40:57 -0500

    email regex validation

--- script.js
+++ script.js
@@ -6,44 +6,54 @@ const password2 = document.getElementById("password2");
 
 // Show input error message
 function showError(input, message) {
-    const formControl = input.parentElement
-    formControl.className = 'form-control error'
-    const small = formControl.querySelector('small')
-    small.innerText = message
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
 }
 
 // show success outline
 function showSuccess(input) {
-    const formControl = input.parentElement
-    formControl.className = 'form-control success'
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+// check email validation
+
+function isValidEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
 // Event listeners
 
-form.addEventListener('submit', function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if (name.value === '') {
-      showError(name, "Your name is required")
+  if (name.value === "") {
+    showError(name, "Your name is required");
   } else {
-      showSuccess(name)
+    showSuccess(name);
   }
 
-  if (email.value === '') {
-    showError(email, "Your email is required")
-} else {
-    showSuccess(email)
-}
+  if (email.value === "") {
+    showError(email, "Your email is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not valid");
+  } else {
+    showSuccess(email);
+  }
 
-if (password.value === '') {
-    showError(password, "Your password is required")
-} else {
-    showSuccess(password)
-}
+  if (password.value === "") {
+    showError(password, "Your password is required");
+  } else {
+    showSuccess(password);
+  }
 
-if (password2.value === '') {
-    showError(password2, "Confirm password required")
-} else {
-    showSuccess(password2)
-}
+  if (password2.value === "") {
+    showError(password2, "Confirm password required");
+  } else {
+    showSuccess(password2);
+  }
 });
